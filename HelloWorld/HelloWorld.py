e50465f0af931791d651658ea61f0d193e6f7ecb
suhail01101984 <suhailmemon84@gmail.com>
2023-05-07 20:46:59 -0600

    updated helloworld

--- HelloWorld/HelloWorld.py
+++ HelloWorld/HelloWorld.py
@@ -1,1 +1,18 @@
-print('HelloWorld')
+#print('HelloWorld')
+
+def helloworld():
+    print("helloworld")
+
+helloworld()
+
+def greeting(name):
+    print("hi "+name+" how are you doing?")
+
+greeting("suhail")
+
+def addtwonumbers(num1,num2):
+    #print(num1+num2)
+    return(num1+num2)
+
+sum=addtwonumbers(9,3)
+print(sum)
